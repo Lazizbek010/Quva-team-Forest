79dde078249f5e912a12080106ac30e2e84e96f0
Toshmatovich <developerjamshidbek111@gmail.com>
2023-02-01 12:58:58 +0500

    yangi

---
+++ jsFile/dinamik.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    document.getElementById("container").style.display = "none"
+    document.getElementById("contend").style.display = "block"
+     bgImg = document.querySelector('.photo')
+}
+bgImg.addEventListener('click', () => {
+    bgImg.style.display = "none"
+})
+$(window).load(function () {
+    $('#loading').fadeOud();
+    $("#loading-img").delay(150).fadeOud(' slow');
+
+}), 10000;
+
